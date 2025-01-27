aa5e16af184daa18b8cdb73320197dac76504e21
jack carter <jsc8443@g.rit.edu>
2025-01-27 13:30:50 -0500

    error removed, recommit for CI

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const { json } = require('stream/consumers');
 const htmlHandler = require('./htmlResponses.js');
 const textHandler = require('./textResponses.js');
 const jsonHandler = require('./jsonResponses.js');
